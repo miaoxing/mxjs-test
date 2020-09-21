af591de723151940c6a77e0228204817d79f3e9f
twinh <twinhuang@qq.com>
2020-09-21 10:45:41 +0800

    feat: 增加 mxjs/test, 提供常用的测试工具方法

---
+++ index.js
@@ -0,0 +1,36 @@
+export function createPromise() {
+  let res, rej;
+
+  const promise = new Promise((resolve, reject) => {
+    res = (result) => {
+      resolve(result);
+      return promise;
+    };
+    rej = (result) => {
+      reject(result);
+      return promise;
+    };
+  });
+
+  promise.resolve = res;
+  promise.reject = rej;
+
+  return promise;
+}
+
+const originalLocation = window.location;
+
+export function setUrl(path) {
+  const url = new URL(path, 'http://localhost/');
+
+  delete window.location;
+  window.location = {
+    pathname: url.pathname,
+    href: url.href,
+    search: url.search,
+  };
+}
+
+export function resetUrl() {
+  window.location = originalLocation;
+}
